7dd44db51654d4de1dd19d02c88e3fb9330f4ca3
Gero Klinkmann <gklinkmann@k-oo.de>
2015-10-20 18:36:27 +0200

    add excel and csv database export excel export using apache poi (see ivy dependency management)

---
+++ src/de/koo/groovy/db/SqlQuery.groovy
@@ -0,0 +1,92 @@
+package de.koo.groovy.db
+import groovy.sql.Sql
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import org.apache.poi.ss.usermodel.CreationHelper
+import org.apache.poi.ss.usermodel.Row
+import org.apache.poi.ss.usermodel.Sheet
+import org.apache.poi.ss.usermodel.Workbook
+
+class SqlQuery {
+	def sql
+	
+	
+	public SqlQuery() {
+		def properties=new Properties()
+		new File("groovy-foo.properties").withInputStream { 
+			properties.load(it)
+		}
+		def config=new ConfigSlurper().parse(properties)
+		
+		sql = Sql.newInstance(config.de.koo.groovy.db.url
+			, config.de.koo.groovy.db.username
+			, config.de.koo.groovy.db.password
+			, config.de.koo.groovy.db.driver)
+	}
+	
+	public exportCsv (query, filename) {
+		new File(filename).withWriter {out->
+			def printColNames = { meta ->
+				def columnNames= (1..meta.columnCount).collect {
+					meta.getColumnLabel(it)
+				}
+				out.writeLine columnNames.join(";")
+			}
+			
+			def printRow = { row -> 
+				def values= row.toRowResult().values().collect { value->
+					cleanValue(value)
+				}
+				out.writeLine values.join(";")
+			}
+			
+			sql.eachRow(query, printColNames, printRow)
+		}
+	}
+	
+	public exportExcel (query,filename){
+		Workbook wb = new HSSFWorkbook()
+		CreationHelper createHelper = wb.getCreationHelper()
+		Sheet sheet = wb.createSheet("sheet 1")
+
+		def printColNames = { meta ->
+			Row row = sheet.createRow((short)0)
+			(1..meta.columnCount).each {
+				row.createCell(it-1).setCellValue(
+					createHelper.createRichTextString(meta.getColumnLabel(it))
+				)
+			}
+		}
+
+		def rowIndex=1
+		def printRow = { row -> 
+			Row excelRow = sheet.createRow((short)rowIndex)
+
+			def cellIndex=0
+			row.toRowResult().values().each { value->
+				excelRow.createCell(cellIndex).setCellValue(
+					createHelper.createRichTextString(cleanValue(value).toString())
+				)
+				cellIndex++
+			}
+			rowIndex++
+		}
+		sql.eachRow(query, printColNames, printRow)
+		
+		FileOutputStream fileOut = new FileOutputStream(filename)
+		wb.write(fileOut)
+		fileOut.close()
+	}
+	
+	private cleanValue (value) {
+		value = (value!=null?value:"")
+		
+		if (value.class.name=="java.lang.String") {
+			value=(value =~ /(\r\n)+/).replaceAll(" ")
+			value=(value =~ /(\n)+/).replaceAll(" ")
+			value=value.replaceAll(";",",")
+		}
+		
+		return value
+	}
+}
---
+++ test/de/koo/test/groovy/db/SqlQueryTest.groovy
@@ -0,0 +1,30 @@
+package de.koo.test.groovy.db
+
+import static org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+import de.koo.groovy.db.SqlQuery
+
+class SqlQueryTest {
+	def query
+	
+	@Before
+	public void setUp() {
+		query=new SqlQuery()
+	}
+
+	@Test
+	public void exportCsv() {
+		def _query="select * from tables limit 3";
+		query.exportCsv(_query,"data/out/results.csv")
+	}
+
+	@Test
+	public void exportExcel() {
+		def _query="select * from tables limit 3";
+		query.exportExcel(_query,"data/out/results.xls")
+	}
+
+}
